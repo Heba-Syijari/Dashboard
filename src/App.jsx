97ccb7619e9b0a39d7ba2fa5a2730f93d0985244
Heba <hsyijari@innovura.io>
2025-01-01 04:42:46 +0300

    Rechart

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
 import Topbar from "./components/topbar/topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./pages/home/Home";
 function App() {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Home />
+      </div>
     </div>
   );
 }
